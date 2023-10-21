afc943ac525c3612b82be5de8a0d6838fdb072d7
Sushanta Bhowmick <bhosushanta922@gmail.com>
2023-10-21 19:15:54 +0530

    add login page

--- client/screens/Home.jsx
+++ client/screens/Home.jsx
@@ -1,114 +1,126 @@
-import { View, Text,SafeAreaView,Platform,StatusBar,StyleSheet, TouchableOpacity } from 'react-native'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView
+} from 'react-native'
 import React, { useState } from 'react'
 import Task from '../components/Task'
 import Icon from 'react-native-vector-icons/Entypo'
-import {Dialog,Button, TextInput} from 'react-native-paper'
+import { Dialog, Button, TextInput } from 'react-native-paper'
 
-const Home = ({navigation}) => {
-    const tasks = [{
-      title:"Task1",description:"Sample Task" ,completed:false,_id:"kushbhidu"
+const Home = ({ navigation }) => {
+  const tasks = [
+    {
+      title: "Task1", description: "Sample Task", completed: false, _id: "kushbhidu"
     },
-  {
-    title:"Task2",description:"Sample Task" ,completed:true,_id:"sdfhfiuho"
-
-  }
+    {
+      title: "Task2", description: "Sample Task", completed: true, _id: "sdfhfiuho"
+    },
+    
   ];
-const [title,setTitle]=useState('')
-const [description,setDescription]=useState('')
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
 
-  const [openDialog,setOpenDialog] =useState(false)
-  const hiDialog=()=>{
+  const [openDialog, setOpenDialog] = useState(false)
+  const hiDialog = () => {
     setOpenDialog(!openDialog)
   }
-  const addTaskHandler=()=>{
+  const addTaskHandler = () => {
     console.log(title)
     console.log(description)
   }
   return (
     <>
-      <View style={{backgroundColor:'#fff',flex:1,paddingTop:Platform.OS === 'android'?StatusBar.currentHeight:0}}>
-    <SafeAreaView>
-      <Text style={styles.heading} >
-       All Tasks
-      </Text>
-      {
-        tasks.map((item)=>(
-          <Task key={item._id}
-          title={item.title}
-          description={item.description}
-          status={item.completed}
-          taskId={item._id}
-           />
-        ))
-      }
-      <TouchableOpacity 
-          style={styles.addBtn}
-          onPress={hiDialog}
-      >
-        <Icon name='add-to-list' size={20}color={"#900"}
-        />
-      </TouchableOpacity>
-    </SafeAreaView>
-    </View>
-    <Dialog visible={openDialog}onDismiss={hiDialog}>
-      <Dialog.Title>ADD A TASk</Dialog.Title>
-      <Dialog.Content>
-        <TextInput 
-          style={styles.input}
-          placeholder='Title'
-          value={title}
-          onChangeText={setTitle}
-        />
-        <TextInput 
-          style={styles.input}
-          placeholder='Description'
-          value={description}
-          onChangeText={setDescription}
-        />
-        <View style={{flexDirection:'row',alignItems:'center'}}>
-      <TouchableOpacity onPress={hiDialog}>
-      <Text>CANCEL</Text>
-      </TouchableOpacity>
-      <Button textColor='#900'
-      onPress={addTaskHandler}
-      >ADD</Button>
-        </View>
-      </Dialog.Content>
-    </Dialog>
+      <View style={{ backgroundColor: '#fff', flex: 1, paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 }}>
+        <ScrollView>
+        <SafeAreaView>
+          <Text style={styles.heading} >
+            All Tasks
+          </Text>
+          {
+            tasks.map((item) => (
+              <Task key={item._id}
+                title={item.title}
+                description={item.description}
+                status={item.completed}
+                taskId={item._id}
+              />
+            ))
+          }
+          <TouchableOpacity
+            style={styles.addBtn}
+            onPress={hiDialog}
+          >
+            <Icon name='add-to-list' size={20} color={"#900"}
+            />
+          </TouchableOpacity>
+        </SafeAreaView>
+        </ScrollView>
+      </View>
+      <Dialog visible={openDialog} onDismiss={hiDialog}>
+        <Dialog.Title>ADD A TASk</Dialog.Title>
+        <Dialog.Content>
+          <TextInput
+            style={styles.input}
+            placeholder='Title'
+            value={title}
+            onChangeText={setTitle}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Description'
+            value={description}
+            onChangeText={setDescription}
+          />
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <TouchableOpacity onPress={hiDialog}>
+              <Text>CANCEL</Text>
+            </TouchableOpacity>
+            <Button textColor='#900'
+              onPress={addTaskHandler}
+            >ADD</Button>
+          </View>
+        </Dialog.Content>
+      </Dialog>
     </>
   )
 }
 
 export default Home
 
-const styles= StyleSheet.create({
-  heading:{
-    fontSize:28,
-    textAlign:'center',
-    marginTop:25,
-    color:'#fff',
-    backgroundColor:'#474747'
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 28,
+    textAlign: 'center',
+    marginTop: 25,
+    color: '#fff',
+    backgroundColor: '#474747'
   },
-  addBtn:{
-    backgroundColor:'white',
-    borderRadius:100,
-    elevation:5,
-    width:150,
-    height:50,
-    justifyContent:'center',
-    alignItems:'center',
-    alignSelf:'center',
-    marginVertical:20,
-  
+  addBtn: {
+    backgroundColor: 'white',
+    borderRadius: 100,
+    elevation: 5,
+    width: 150,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    marginVertical: 20,
+
   },
-  input:{
-    backgroundColor:'white',
-    borderWidth:1,
-    borderColor:"#b5b5b5",
-    padding:5,
-    paddingLeft:15,
-    borderRadius:10,
-    marginVertical:15,
-    fontSize:15,
+  input: {
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: "#b5b5b5",
+    padding: 5,
+    paddingLeft: 15,
+    borderRadius: 10,
+    marginVertical: 15,
+    fontSize: 15,
   }
 })
--- client/screens/Login.jsx
+++ client/screens/Login.jsx
@@ -1,12 +1,86 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { Button, TextInput } from 'react-native-paper'
 
-const Login = () => {
+const Login = ({navigation}) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  const loginHandler =()=>{
+    console.log("Welcome back users")
+  }
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <Text style={{ fontSize: 20, margin: 20 }} >Welcome</Text>
+      <View style={{ width: "70%" }}>
+        <TextInput
+          style={styles.input}
+          placeholder='Email'
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          secureTextEntry
+          style={styles.input}
+          placeholder='Password'
+          value={password}
+          onChangeText={setPassword}
+        />
+
+      </View>
+      <Button
+        disabled={!email || !password}
+        style={styles.btn}
+        onPress={loginHandler}> <Text style={{color:'#fff'}}>Login</Text></Button>
+      <Text
+      style={{marginTop:20}}
+      >
+        Or
+      </Text>
+      <TouchableOpacity onPress={()=>navigation.navigate('register')}>
+        <Text
+        style={{
+          color:"#900",
+          height:20,
+          margin:20
+        }}
+        >
+          Sign Up
+        </Text>
+      </TouchableOpacity>
+      </View>
   )
 }
 
+
 export default Login
+
+const styles = StyleSheet.create({
+  btn: {
+    backgroundColor:'#900',
+    borderRadius:0,
+    padding:5,
+    width:'70%'
+ 
+   },
+   input: {
+     backgroundColor: 'white',
+     borderWidth: 1,
+     borderColor: "#b5b5b5",
+     padding: 5,
+     paddingLeft: 15,
+     borderRadius: 10,
+     marginVertical: 15,
+     fontSize: 15,
+     
+     
+   }
+ })
